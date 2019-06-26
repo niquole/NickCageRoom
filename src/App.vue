5915db59fe6c8af628ee6a43deb717ae232e2f62
niquole <nicole.antonopoulou@gmail.com>
2019-06-26 12:13:21 +0200

    background

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nick/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import nick from './components/nick.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    nick
+  },
+  data() {
+    return {
+      x: "",
+      y: ""
+
+    }
+  },
+  methods: {
+    move: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    }
   }
 }
 </script>
@@ -21,8 +34,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/nick.vue
@@ -0,0 +1,29 @@
+<template>
+    <div @click="move">
+        <img src="../assets/nick.png" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'nick'
+
+}
+</script>
+
+<style>
+body {
+    margin: 0;
+}
+div {
+    height: 100vh;
+    background-image: url('http://s3-us-west-2.amazonaws.com/capitalradio-code/wp-content/uploads/2019/05/fire.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+div img {
+    position: absolute;
+}
+</style>
+
